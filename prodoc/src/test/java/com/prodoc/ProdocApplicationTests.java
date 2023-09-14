a32713914f2743e93f8e48de8168c210b1ed7028
admin <admin@DESKTOP-H97OAL1>
2023-09-14 17:44:01 +0900

    9.14 / 로그인,회원가입:학원작업 / 김은주

--- prodoc/src/test/java/com/prodoc/ProdocApplicationTests.java
+++ prodoc/src/test/java/com/prodoc/ProdocApplicationTests.java
@@ -1,18 +1,32 @@
 package com.prodoc;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+
 import org.jasypt.encryption.StringEncryptor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.prodoc.user.service.EmailService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootTest
 class ProdocApplicationTests {
 
 	@Autowired
+	private EmailService emailService;
+	@Autowired
 	StringEncryptor jasyptStringEncryptor;
 	@Test
 	void contextLoads() {
-		String[] datas = {""};
+		String[] datas = {
+				"smtp.gmail.com",
+				"587",
+				"8520hana@gmail.com",
+				"truoozlqqvoupwll"
+				};
 		
 		for(String data : datas) {
 			String enyData = jasyptStringEncryptor.encrypt(data);
