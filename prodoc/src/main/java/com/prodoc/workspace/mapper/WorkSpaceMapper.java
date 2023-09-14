f94cafd65620e74c38e7ef24e8b9e14cf1a60ab2
admin <end921@naver.com>
2023-09-14 17:04:47 +0900

    20230914 김시인 일단 팀워크스페이스일 경우 초대하는거 빼고 등록기능은 됨(하드코딩)

--- prodoc/src/main/java/com/prodoc/workspace/mapper/WorkSpaceMapper.java
+++ prodoc/src/main/java/com/prodoc/workspace/mapper/WorkSpaceMapper.java
@@ -6,5 +6,14 @@ import com.prodoc.workspace.service.WorkSpaceVO;
 
 public interface WorkSpaceMapper {
 	public List<String> selectWorkNo(WorkSpaceVO workVO);
-}
 
+	// 워크스페이스 등록
+	public void registerWorkspace(WorkSpaceVO workVO);
+
+	// 워크스페이스 수정(덜됨)
+	public boolean modifyWorkspace(WorkSpaceVO workVO);
+	// 팀스페이스인 경우 소유자 교체(덜만듦)
+	public void renewOWNER(WorkSpaceVO workVO);
+
+	// 워크스페이스 삭제
+}
--- prodoc/src/test/java/com/prodoc/ProdocApplicationTests.java
+++ prodoc/src/test/java/com/prodoc/ProdocApplicationTests.java
@@ -1,23 +1,40 @@
 package com.prodoc;
 
-import org.jasypt.encryption.StringEncryptor;
+//import org.jasypt.encryption.StringEncryptor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.prodoc.workspace.service.WorkSpaceService;
+import com.prodoc.workspace.service.WorkSpaceVO;
+
 @SpringBootTest
 class ProdocApplicationTests {
 
+//	@Autowired
+//	StringEncryptor jasyptStringEncryptor;
+//	@Test
+//	void contextLoads() {
+//		String[] datas = {""};
+//		
+//		for(String data : datas) {
+//			String enyData = jasyptStringEncryptor.encrypt(data);
+//			System.out.println(enyData);
+//		}
+//	}
+	
 	@Autowired
-	StringEncryptor jasyptStringEncryptor;
+	WorkSpaceService workspaceService;
+	
 	@Test
-	void contextLoads() {
-		String[] datas = {""};
-		
-		for(String data : datas) {
-			String enyData = jasyptStringEncryptor.encrypt(data);
-			System.out.println(enyData);
-		}
+	public void insertTest(){
+		WorkSpaceVO vo = new WorkSpaceVO();
+		//, , , 
+		vo.setWorkType("PERSONAL");
+		vo.setWorkName("서비스 테스트");
+		vo.setPublicCheck("PUBLIC");
+		vo.setEmail("serviceTest@naver.com");
+		workspaceService.insertWorkspace(vo);
 	}
 
 }
