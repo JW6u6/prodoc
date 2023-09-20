c52179935c9820f275e877cf8db2901ab436088d
admin <admin@DESKTOP-H97OAL1.mshome.net>
2023-09-20 17:51:32 +0900

    09.20 17:51 / 회원정보 수정 & 카카오 연동 일부 / 김은주

--- prodoc/src/main/java/com/prodoc/user/service/impl/UserServiceImpl.java
+++ prodoc/src/main/java/com/prodoc/user/service/impl/UserServiceImpl.java
@@ -1,5 +1,8 @@
 package com.prodoc.user.service.impl;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -8,6 +11,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.prodoc.member.mapper.MemberMapper;
 import com.prodoc.user.mapper.UserMapper;
 import com.prodoc.user.service.UserService;
@@ -24,9 +29,12 @@ public class UserServiceImpl implements UserService, UserDetailsService{
 	MemberMapper mMapper;
 	@Autowired
 	PasswordEncoder passwordEncoder;
-	
+
+    //@Autowired
+   // private AuthenticationManager authenticationManager; // 얘를 통해 세션값 변경해야함
+
 	@Override
-	public UserVO getUser(UserVO user) {	//로그인
+	public UserVO getUser(UserVO user) {	//로그인 OR 기존 메일 찾기
 		return mapper.selectUser(user);	
 	}
 
@@ -56,6 +64,28 @@ public class UserServiceImpl implements UserService, UserDetailsService{
 			return 0;
 		}
 		return mapper.deleteUser(user);
+	}
+
+	@Override
+	@Transactional
+	public String modifyInfo(UserVO user, HttpServletRequest request) {
+		if(!user.getPassword().equals(""))	//비밀번호 변경이 있음 -> 암호화 진행
+			user.setPassword(passwordEncoder.encode(user.getPassword()));
+		
+		ObjectMapper objectMapper = new ObjectMapper();
+		String json = "";
+		try {
+			json = objectMapper.writeValueAsString(user);
+			System.out.println(json);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		if(	mapper.updateUser(user) > 0 ){	//업데이트 진행
+			HttpSession session = request.getSession();
+			session.setAttribute("logUser", mapper.selectUser(user));	//세션 재등록
+			return "{\"result\":true, \"data\" : "+ json +"}";
+		}
+		return "{\"result\":false}";
 	}
 
 }
