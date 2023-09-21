feea3dcd56e50684c8253198ab98ea0ddaaf84c1
admin <admin@DESKTOP-CB2V3Q9>
2023-09-21 17:48:48 +0900

    0921

--- prodoc/src/main/java/com/prodoc/block/mapper/BlockMapper.java
+++ prodoc/src/main/java/com/prodoc/block/mapper/BlockMapper.java
@@ -5,16 +5,18 @@ import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 import com.prodoc.block.service.BlockVO;
+import com.prodoc.block.service.BookMarkVO;
 
 @Mapper
 public interface BlockMapper {
 	public List<BlockVO> selectAllBlock(BlockVO blockVO);
+	public BlockVO selectBlock(BlockVO blockVO);
 	public int insertBlock(BlockVO blockVO);
 	public int updateBlock(BlockVO blockVO);
 	public int deleteBlock(BlockVO blockVO);
 	public int createCheckBlock(Map<String,String> map);
-	public int createBookMark(String displayId);
-	public int updateBookMark(Map<String,String> map);
-	public String getBookMark(String displayId);
-	public int deleteBookMark(String displayId);
+	public int createBookMark(BookMarkVO bookmarkVO);
+	public int updateBookMark(BookMarkVO bookmarkVO);
+	public BookMarkVO getBookMark(BookMarkVO bookmarkVO);	
+	public int deleteBookMark(String id);
 }
