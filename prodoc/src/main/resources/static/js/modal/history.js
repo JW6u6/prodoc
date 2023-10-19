d35be503f3cbdd9faf82b4fc11f1f2ccbc9314be
admin <admin@DESKTOP-H97OAL1>
2023-10-19 12:43:47 +0900

    임시커밋

--- prodoc/src/main/resources/static/js/modal/history.js
+++ prodoc/src/main/resources/static/js/modal/history.js
@@ -125,7 +125,7 @@ function settingHistoryResult(history){
 						        <p>${formatTime(new Date(history[i].upDate))}</p>
 						    </div>
 						    <div class="history_item--right">
-						        <p>페이지</p>
+						        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M240-80q-33 0-56.5-23.5T160-160v-640q0-33 23.5-56.5T240-880h320l240 240v480q0 33-23.5 56.5T720-80H240Zm280-520v-200H240v640h480v-440H520ZM240-800v200-200 640-640Z"/></svg>
 						        <h3>${history[i].pageName}<span>(${history[i].workName})</span></h3>
 						        <p>${history[i].nickname}(${history[i].creUser})</p>
 						    </div>`;
@@ -141,9 +141,9 @@ function settingHistoryResult(history){
 						        <p>${formatTime(new Date(history[i].upDate))}</p>
 						    </div>
 						    <div class="history_item--right">
-						    	<p>워크스페이스</p>
+						    	<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h240l80 80h320q33 0 56.5 23.5T880-640v400q0 33-23.5 56.5T800-160H160Zm0-80h640v-400H447l-80-80H160v480Zm0 0v-480 480Z"/></svg>
+						    	<p>${history[i].nickname}(${history[i].creUser})</p>
 						        <h3>${history[i].workName}</h3>
-						        <p>${history[i].nickname}(${history[i].creUser})</p>
 						    </div>`
 						    
 			if(history[i].historyType == 'DELETE'){
