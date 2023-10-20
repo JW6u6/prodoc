21ba1bebb8728cb88728fb574bfc06d245b88093
admin <admin@DESKTOP-H97OAL1>
2023-10-20 10:43:57 +0900

    로그인 결함 수정 / 김은주

--- prodoc/src/main/java/com/prodoc/user/service/UserService.java
+++ prodoc/src/main/java/com/prodoc/user/service/UserService.java
@@ -1,10 +1,13 @@
 package com.prodoc.user.service;
 
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 
 public interface UserService {
 	public UserVO getUser(UserVO user); //특정(단일) 유저 검색 *default 조건: email
-	public UserVO getFind(UserVO user); //특정(단일) 유저 검색 *default 조건: phone
+	public List<String> getFind(UserVO user); //특정(단일) 유저 검색 *default 조건: phone
 	public int join(UserVO user); 		//이메일 회원가입
 	public int joinOut(UserVO user);	//탈퇴
 	public String modifyInfo(UserVO user, HttpServletRequest request); //유저 정보 수정
