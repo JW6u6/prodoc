7a222088bbf06ceb9229d199b3604b631a2d1f0f
admin <admin@DESKTOP-JNDUJRL.mshome.net>
2023-10-23 14:29:44 +0900

    머지해주세요부탁드립ㄴ디ㅏ

--- prodoc/src/main/resources/static/js/db/dbEvent.js
+++ prodoc/src/main/resources/static/js/db/dbEvent.js
@@ -143,17 +143,15 @@ async function getChildList(disId){
                         const selectOp = DBele.querySelector(".DBS_option");
                         const attrList = await getUseAttrList(disId);
                         selectOp.innerHTML = '';
-                        for(let i=0; i<attrList.length+1; i++){
+                        let attrDiv = document.createElement("option");
+                        attrDiv.innerText = '이름';
+                        attrDiv.value = "";
+                        selectOp.append(attrDiv);
+                        for(let i=0; i<attrList.length; i++){
                             if (attrList[i].attrId !== 'CAL'){
                                 let attrDiv = document.createElement("option");
-                                if(i===0) {
-                                    attrDiv.innerText = '이름';
-                                    attrDiv.value = "";
-                                }
-                                else {
-                                    attrDiv.innerText = attrList[i].attrName;
-                                    attrDiv.value = attrList[i].dbUseId;
-                                }
+                                attrDiv.innerText = attrList[i].attrName;
+                                attrDiv.value = attrList[i].dbUseId;
                                 selectOp.append(attrDiv);
                             }
                         }
