125e1fe5e38c8f942f4b185c1b3f54b0ed6878af
admin <admin@DESKTOP-CB2V3Q9>
2023-10-23 17:52:17 +0900

    url

--- prodoc/src/main/resources/static/js/app.js
+++ prodoc/src/main/resources/static/js/app.js
@@ -1,6 +1,6 @@
 //메인이 될 IP 주소 설정
 const stompClient = new StompJs.Client({
-  brokerURL: "ws://prodox.me/websocket",
+  brokerURL: "wss://prodox.me/websocket",
 });
 console.log(stompClient);
 connect();  
